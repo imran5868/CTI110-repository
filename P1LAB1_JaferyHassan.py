ab37fbba477b3fe1f50c245da4561badaa0d68a1
Imran Hassan Jafery <129028017+abubakar5868@users.noreply.github.com>
2024-10-01 12:44:11 +0500

    Add files via upload

---
+++ P1LAB1_JaferyHassan.py
@@ -0,0 +1,16 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Your Name
+... # Date
+... # Assignment Name
+... # A brief description of the project
+... 
+... # This program asks the user for their first name and last name,
+... # then displays a welcome message including their full name.
+... 
+... # Prompt the user to enter their first and last name separately
+... first_name = input("Please enter your first name: ")
+... last_name = input("Please enter your last name: ")
+... 
+... # Display a welcome message
+... print(f"Welcome, {first_name} {last_name}, to this course!")
